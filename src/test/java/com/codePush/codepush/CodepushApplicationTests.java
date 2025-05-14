e1042a4ba8b316b5f7ae2a4f4d683022dc74ec1e
Yogesh Sharma <sharmayogesh5star@gmail.com>
2025-05-14 15:14:36 +0530

    initial CI/CD setup with Dockerfile and Jenkinsfile

---
+++ src/test/java/com/codePush/codepush/CodepushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codePush.codepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
